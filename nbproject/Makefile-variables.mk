529eb53c10ba440f59f1269a159c68f8e65983c2
Iscle <albertiscle9@gmail.com>
2024-01-11 00:22:00 +0100

    Initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=foster_oven_psu.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/foster_oven_psu.X.production.hex
